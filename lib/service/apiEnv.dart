7dd37596b074161b560d00aa25ac7b504904905d
Trần Viết Đông <xhunter1412@gmail.com>
2020-03-20 21:20:39 +0700

    config call api local

--- lib/service/apiEnv.dart
+++ lib/service/apiEnv.dart
@@ -1,6 +1,9 @@
 class Env {
 //  static String endPoint = "http://45.119.83.107:9002/";
-  static String endPoint = "http://dongtv.hisoft.vn/";
-  static String imageEndPoint = "http://dongtv.hisoft.vn/api/File/Image?imageUrl=";
+//  static String endPoint = "http://dongtv.hisoft.vn/";
+  static String endPoint = "http://10.0.2.2:58345/";
+
+//  static String imageEndPoint = "http://dongtv.hisoft.vn/api/File/Image?imageUrl=";
+  static String imageEndPoint = "http://10.0.2.2:58345/api/File/Image?imageUrl=";
 
 }
